650884dbf7477f30f5600af1f0f87223646120b8
nearnshaw <nearnshaw@decentraland.org>
2021-02-18 18:38:11 -0300

    default behavior from time API, not server

--- src/game.ts
+++ src/game.ts
@@ -1,13 +1,10 @@
 import { scene } from './modules/scene'
 import { spawnTables } from './modules/tables'
-import * as utils from '@dcl/ecs-scene-utils'
+
 import { movePlayerTo } from '@decentraland/RestrictedActions'
 import { initiateVJUI } from './modules/adminVJ'
-import * as ui from '@dcl/ui-scene-utils'
-import { NPC } from '@dcl/npc-scene-utils'
-import { elf1Intro } from './modules/dialog'
+
 import { checkTime } from './modules/showPlaying'
-import { Synced } from './modules/syncable'
 
 let shroomShape = new GLTFShape('models/shroom_building.glb')
 
@@ -39,36 +36,6 @@ Input.instance.subscribe('BUTTON_DOWN', ActionButton.PRIMARY, true, (e) => {
     `) },`
   )
 })
-
-export let elf1 = new NPC(
-  { position: new Vector3(28, 0, 32), scale: new Vector3(6, 6, 6) },
-  'models/ElfBell.glb',
-  () => {
-    elf1.talk(elf1Intro)
-    elf1.playAnimation(`Greet`, true, 2.17)
-  },
-  {
-    idleAnim: `Idle_01`,
-    faceUser: true,
-    darkUI: true,
-    onlyETrigger: true,
-  }
-)
-
-export let elf2 = new NPC(
-  { position: new Vector3(40, 0, 37), scale: new Vector3(6, 6, 6) },
-  'models/ElfGirl.glb',
-  () => {
-    elf2.talk(elf1Intro)
-    elf2.playAnimation(`Greet`, true, 2.17)
-  },
-  {
-    idleAnim: `Idle_01`,
-    faceUser: true,
-    darkUI: true,
-    onlyETrigger: true,
-  }
-)
 
 // start default light show
 checkTime()
